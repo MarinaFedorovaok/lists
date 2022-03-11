befc0b50e62431e37aa7142a7b3011febb798ae9
MarinaFedorovaok <fedma001@mail.ru>
2022-03-11 22:32:15 +0300

    all proxies bad

--- with_file.py
+++ with_file.py
@@ -3,7 +3,8 @@ import pycurl
 file = open('pycurl.md','wb') 
 
 crl = pycurl.Curl() 
-crl.setopt(crl.URL, 'https://wiki.python.org/moin/BeginnersGuide') 
+#crl.setopt(crl.URL, 'https://wiki.python.org/moin/BeginnersGuide') 
+crl.setopt(crl.URL, 'https://www.proxy-list.download/api/v1/get?type=http')
 crl.setopt(crl.WRITEDATA, file) 
 crl.perform() 
 crl.close() 
